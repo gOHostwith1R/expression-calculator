14613754853e9ddc6f8f9081ccc9c2a507f6c678
gOHostwith1R <57642445+gOHostwith1R@users.noreply.github.com>
2020-03-17 16:49:27 +0300

    init:test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function eval() {
+    // Do not use eval!!!
+    return;
+}
+
+function expressionCalculator(expr) {
+    // write your solution here
+    console.log("hello");
+}
+
+module.exports = {
+    expressionCalculator
+}
